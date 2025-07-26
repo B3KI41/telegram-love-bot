e44b71ee3e67eefc0be221e9a8454738a976fa6f
B3KI41 <ualekumnihao@gmail.com>
2025-07-26 16:17:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, InputMediaPhoto, InputMediaVideo
 from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandler, ContextTypes
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "8222070472:AAHHZEU9SJISEMmmXvvCMHeUSPXcZy_JhO0"
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
